4427982ba9f2c3585502b29b71cbe1c8c0eafe7e
Kjell Dießel <development@kettil.de>
2021-11-04 23:25:24 +0100

    fix: log output

--- packages/package-app/src/lib/app.ts
+++ packages/package-app/src/lib/app.ts
@@ -4,9 +4,7 @@ import { Logger } from 'pino';
 const app = async ({ env, log }: { env: NodeJS.ProcessEnv; log: Logger }): Promise<void> => {
   log.debug({ env }, 'ENV');
 
-  log.info({ solution }, 'foo 1');
-  log.info({ secondSolution }, 'foo 2');
-  log.info({ thirdSolution }, 'foo 3');
+  log.info({ solution, secondSolution, thirdSolution }, 'foo');
 
   // dummy await for eslint
   await new Promise((r) => setTimeout(r, 2000));

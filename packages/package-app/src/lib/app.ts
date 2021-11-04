97e1fdb13618e0f366a309cddfd6d66ecc40968f
Kjell Dießel <development@kettil.de>
2021-11-04 22:06:04 +0100

    feat: add lib

--- packages/package-app/src/lib/app.ts
+++ packages/package-app/src/lib/app.ts
@@ -1,7 +1,12 @@
+import { solution, secondSolution, thirdSolution } from '@flytrx/test-lib';
 import { Logger } from 'pino';
 
 const app = async ({ env, log }: { env: NodeJS.ProcessEnv; log: Logger }): Promise<void> => {
   log.debug({ env }, 'ENV');
+
+  log.info({ solution }, 'foo 1');
+  log.info({ secondSolution }, 'foo 2');
+  log.info({ thirdSolution }, 'foo 3');
 
   // dummy await for eslint
   await new Promise((r) => setTimeout(r, 2000));

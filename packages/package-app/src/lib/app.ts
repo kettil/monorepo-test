4ab705e1f1665e89707dca0ab17a3aa2dcfe635a
Kjell Dießel <development@kettil.de>
2021-11-08 17:48:51 +0100

    fix: update app

--- packages/package-app/src/lib/app.ts
+++ packages/package-app/src/lib/app.ts
@@ -4,7 +4,11 @@ import { Logger } from 'pino';
 const app = async ({ env, log }: { env: NodeJS.ProcessEnv; log: Logger }): Promise<void> => {
   log.debug({ env }, 'ENV');
 
-  log.info({ solution, secondSolution, thirdSolution }, 'foo...');
+  log.info({
+    solution,
+    secondSolution,
+    thirdSolution,
+  }, 'foo...');
 
   // dummy await for eslint
   await new Promise((r) => setTimeout(r, 2000));

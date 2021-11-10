22524d012e1eb03233e8c488fb3b38861a250118
Kjell Dießel <development@kettil.de>
2021-11-11 00:07:56 +0100

    chore: update ci cleanup script

--- .github/cleanup.sh
+++ .github/cleanup.sh
@@ -5,8 +5,8 @@ sleep 10
 
 npm install --ignore-scripts --audit false
 
-git config --global user.email "${secrets.CI_USER_EMAIL}"
-git config --global user.name "${secrets.CI_USER_NAME}"
+git config --global user.email "${USER_EMAIL}"
+git config --global user.name "${USER_NAME}"
 git add package-lock.json
 git commit -m "chore(release): update package-lock.json [skip ci]"
 git push
